d6c4d3298517e8f184035b4725efa3b92a8305c8
Andrew Petersen <kirbysayshi@gmail.com>
2020-02-02 00:05:03 -0500

    clarify

--- App.tsx
+++ App.tsx
@@ -6,8 +6,15 @@ const test = Asset.fromModule(require('./assets/icon.png'));
 
 // This should log something like: `http://localhost:[port]/assets/icon.png?[long query]
 // but instead is: `http://localhost:[port]/assets/assets/icon.png?[long query]
-// This results in a 404 / error!
+// Curling this results in a 404 / error!
 console.log(test.uri);
+
+// This produces a working, expected URL.
+// Curling this downloads the file.
+const normalized = test.uri.replace(/(assets\/)+/g, 'assets/');
+console.log(normalized);
+
+
 
 export default function App() {
   return (
