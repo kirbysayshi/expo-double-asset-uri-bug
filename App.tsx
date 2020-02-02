c8bb31e19dc16d4ce423bc15366d93172a051d8f
Andrew Petersen <kirbysayshi@gmail.com>
2020-02-01 23:50:05 -0500

    simplify + comment

--- App.tsx
+++ App.tsx
@@ -3,21 +3,16 @@ import { StyleSheet, Text, View } from 'react-native';
 import { Asset } from 'expo-asset';
 
 const test = Asset.fromModule(require('./assets/icon.png'));
+
+// This should log something like: `http://localhost:[port]/assets/icon.png?[long query]
+// but instead is: `http://localhost:[port]/assets/assets/icon.png?[long query]
+// This results in a 404 / error!
 console.log(test.uri);
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <View>
+      <Text>{test.uri}</Text>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
