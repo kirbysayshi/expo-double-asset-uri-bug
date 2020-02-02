6e5eecfa92a7fe71252e82e28fee57afacb59d51
Andrew Petersen <kirbysayshi@gmail.com>
2020-02-02 00:05:26 -0500

    cleanup

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
 import { Asset } from 'expo-asset';
 
 const test = Asset.fromModule(require('./assets/icon.png'));
